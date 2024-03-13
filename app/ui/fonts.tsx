39279bafdbc5200be1798925ab92d3654d77cce4
yabets <yabets328@gmail.com>
2024-03-13 07:39:58 +0300

    1st phase

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -3,6 +3,7 @@ import { Inter, Lusitana } from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
  
 export const lusitana = Lusitana({
-  weight: ['400', '700'],
+  weight: ['400', '700'
+],
   subsets: ['latin'],
 });
